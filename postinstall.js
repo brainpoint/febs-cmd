3eb6a51a229ae1eaf27a33f6decf1b49952e8ec5
lipengxiang <lipengxiang@tjitech.cn>
2019-01-16 23:18:25 +0800

    [feature] package

--- postinstall.js
+++ postinstall.js
@@ -1,6 +1,8 @@
-console.log('');
-console.log('************************');
-console.log('*  run \'febs-cmd\' in terminal.  *');
-console.log('************************');
-console.log('');
-console.log('');
+
+    console.log('');
+    console.log('************************');
+    console.log('*  run \'febs-cmd\' in terminal.  *');
+    console.log('************************');
+    console.log('');
+    console.log('');
+
